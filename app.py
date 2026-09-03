0c99ef5a65cc07df35536b3677701e5381c85206
Gaurav Sharma -20jan <gauravsharma27390@gmail.com>
2026-09-03 12:02:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ if query:
     
     try:
         completion = client.chat.completions.create(
-            model="llama3-8b-8192", 
+            model="llama-3.1-8b-instant", 
             messages=[{"role": "system", "content": system_prompt}, {"role": "user", "content": query}],
             temperature=0.5,
         )
